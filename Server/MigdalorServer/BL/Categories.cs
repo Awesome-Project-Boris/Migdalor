d4637a19d84f1e9af74e2ce2b908297402571464
Tom <tomkovt2000@gmail.com>
2025-03-16 16:27:47 +0200

    Added classes

---
+++ Server/MigdalorServer/BL/Categories.cs
@@ -0,0 +1,14 @@
+﻿namespace MigdalorServer.BL
+{
+    public class Categories
+    {
+        private string categoryName;
+
+        public Categories(string categoryName)
+        {
+            CategoryName = categoryName;
+        }
+
+        public string CategoryName { get => categoryName; set => categoryName = value; }
+    }
+}
---
+++ Server/MigdalorServer/BL/Picture.cs
@@ -0,0 +1,23 @@
+﻿namespace MigdalorServer.BL
+{
+    public class Picture
+    {
+        private int picId;
+        private string picName;
+        private string picPath;
+        private string picAlt;
+
+        public Picture(int picId, string picName, string picPath, string picAlt)
+        {
+            PicId = picId;
+            PicName = picName;
+            PicPath = picPath;
+            PicAlt = picAlt;
+        }
+
+        public int PicId { get => picId; set => picId = value; }
+        public string PicName { get => picName; set => picName = value; }
+        public string PicPath { get => picPath; set => picPath = value; }
+        public string PicAlt { get => picAlt; set => picAlt = value; }
+    }
+}
