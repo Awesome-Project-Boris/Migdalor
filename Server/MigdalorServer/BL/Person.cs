bdc87f897ad0e01aa29c990d740e18f25cf04720
LiorMast <liormasturov@gmail.com>
2025-03-16 16:04:48 +0200

    created person class

---
+++ Server/MigdalorServer/BL/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace MigdalorServer.BL
+{
+    public class Person
+    {
+        private int id;
+        private string hebFirstName;
+        private string hebLastName;
+        private int age;
+
+    }
+}
