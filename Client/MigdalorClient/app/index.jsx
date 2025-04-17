656ab8b4ad0321c10dabe007b089f0485da3a745
LiorMast <liormasturov@gmail.com>
2025-04-17 15:52:58 +0300

    deleted unused import

--- Client/MigdalorClient/app/index.jsx
+++ Client/MigdalorClient/app/index.jsx
@@ -1,6 +1,5 @@
 import { Redirect } from "expo-router";
 import { useAuth } from "@/context/AuthProvider";
-import { View, ActivityIndicator } from "react-native";
 
 export default function Index() {
   const { user } = useAuth();
