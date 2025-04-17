019169b7cffbe64c4bd4147fbd5df806af2d3414
LiorMast <liormasturov@gmail.com>
2025-04-17 20:19:16 +0300

    fetching notices from server

--- Server/MigdalorServer/Models/OhCategory.cs
+++ Server/MigdalorServer/Models/OhCategory.cs
@@ -1,6 +1,20 @@
-﻿namespace MigdalorServer.Models
+﻿using Microsoft.EntityFrameworkCore;
+using MigdalorServer.Database;
+
+namespace MigdalorServer.Models
 {
     public partial class OhCategory
     {
+        public static List<OhCategory> GetAllCategories()
+        {
+            using MigdalorDBContext db = new MigdalorDBContext();
+            return db.OhCategories.ToList();
+        }
+
+        public static OhCategory? GetCategoryByName(string name)
+        {
+            using MigdalorDBContext db = new MigdalorDBContext();
+            return db.OhCategories.FirstOrDefault(c => c.CategoryName == name);
+        }
     }
 }
