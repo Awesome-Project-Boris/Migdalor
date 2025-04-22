2855f98fd145caf48548ff36659dfc6c23daf3b8
LiorMast <liormasturov@gmail.com>
2025-04-22 16:34:45 +0300

    changed code to work with dev tunnel

--- Client/MigdalorClient/app/constants/Globals.jsx
+++ Client/MigdalorClient/app/constants/Globals.jsx
@@ -1,7 +1,8 @@
 import { Dimensions } from "react-native";
 
 export let Globals = {
-  API_BASE_URL: "http://192.168.0.160:5293", // Lior's local server
+  API_BASE_URL: "https://6ztnhfvk-44315.euw.devtunnels.ms", // Dev Tunnel
+  // API_BASE_URL: "http://192.168.0.160:5293", // Lior's local server
   // API_BASE_URL: 'http://192.168.68.107:5293', // Tom's
   // API_BASE_URL: 'http://192.168.7.16:5293', // Roi's
   APP_NAME: "MyCoolApp",
