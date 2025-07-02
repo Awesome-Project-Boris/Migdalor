c122166c7302a8a7403b8817066faa06ffbc8f2f
LiorMast <liormasturov@gmail.com>
2025-07-02 18:25:32 +0300

    added sql tables for interests

---
+++ SQL/OH_InterestCategories.sql
@@ -0,0 +1,5 @@
+CREATE TABLE OH_InterestCategories(
+categoryID int IDENTITY(1,1) PRIMARY KEY,
+categoryHebName nvarchar(50) NOT NULL,
+categoryEngName varchar(50) NOT NULL,
+);
--- SQL/OH_Interests.sql
+++ SQL/OH_Interests.sql
@@ -1,5 +1,6 @@
 CREATE TABLE OH_Interests(
 interestID int identity(1,1) primary key,
+category INT not null references OH_InterestCategories(categoryID),
 hebName nvarchar(50) unique not null,
 engName nvarchar(50) unique not null
 );
