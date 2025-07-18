3eb95fb05fcc41a88e9038a2ff4a6c0a082685e9
LiorMast <liormasturov@gmail.com>
2025-07-18 17:09:37 +0300

    map nodes scaffold

---
+++ Server/MigdalorServer/Models/Generated/OhMapNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MigdalorServer.Models
+{
+    [Table("OH_MapNodes")]
+    public partial class OhMapNode
+    {
+        public OhMapNode()
+        {
+            Buildings = new HashSet<OhBuilding>();
+        }
+
+        [Key]
+        [Column("NodeID")]
+        public int NodeId { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        [StringLength(255)]
+        public string? Description { get; set; }
+
+        [ForeignKey("NodeId")]
+        [InverseProperty("Nodes")]
+        public virtual ICollection<OhBuilding> Buildings { get; set; }
+    }
+}
