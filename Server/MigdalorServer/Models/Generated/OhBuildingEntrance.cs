16bd97f304666bb1402d475e6394f114cc385d91
RoiCoDA <112205767+RoiCoDA@users.noreply.github.com>
2025-07-19 23:09:13 +0300

    redo building entrances

--- Server/MigdalorServer/Models/Generated/OhBuildingEntrance.cs
+++ Server/MigdalorServer/Models/Generated/OhBuildingEntrance.cs
@@ -7,24 +7,17 @@ using Microsoft.EntityFrameworkCore;
 namespace MigdalorServer.Models
 {
     [Table("OH_BuildingEntrances")]
-    // The [Index] attribute might also be here, which is fine.
     public partial class OhBuildingEntrance
     {
-        // REMOVE [Key] from here
+        [Key]
         [Column("BuildingID")]
         public Guid BuildingId { get; set; }
-
-        // REMOVE [Key] from here
+        [Key]
         [Column("NodeID")]
         public int NodeId { get; set; }
 
         [ForeignKey("BuildingId")]
         [InverseProperty("OhBuildingEntrances")]
         public virtual OhBuilding Building { get; set; } = null!;
-
-        // You should also have a navigation property to OhMapNode
-        [ForeignKey("NodeId")]
-        [InverseProperty("OhBuildingEntrances")]
-        public virtual OhMapNode Node { get; set; } = null!;
     }
 }
