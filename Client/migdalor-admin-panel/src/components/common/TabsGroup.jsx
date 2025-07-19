0e45a181a1e8b390f7561111116bd7a61e751d76
LiorMast <liormasturov@gmail.com>
2025-07-20 01:21:43 +0300

    added icons to tab group

--- Client/migdalor-admin-panel/src/components/common/TabsGroup.jsx
+++ Client/migdalor-admin-panel/src/components/common/TabsGroup.jsx
@@ -1,6 +1,6 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import React from "react";
-import { cn } from "../../lib/utils"; // Assuming you have a utility for merging class names
+import { cn } from "../../lib/utils";
 
 function TabsGroup({ tabs, listClassName, triggerClassName }) {
   if (!tabs || tabs.length === 0) {
@@ -8,7 +8,7 @@ function TabsGroup({ tabs, listClassName, triggerClassName }) {
   }
 
   const defaultTriggerStyle =
-    "inline-flex items-center w-full justify-center whitespace-nowrap rounded-full px-6 py-2.5 text-base font-semibold text-gray-600 ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-blue-600 data-[state=active]:shadow-md";
+    "inline-flex items-center justify-center whitespace-nowrap rounded-full px-6 py-2.5 text-base font-semibold text-gray-600 ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-blue-600 data-[state=active]:shadow-md";
 
   return (
     <Tabs defaultValue={tabs[0].value}>
@@ -24,6 +24,8 @@ function TabsGroup({ tabs, listClassName, triggerClassName }) {
             value={tab.value}
             className={cn(defaultTriggerStyle, triggerClassName)}
           >
+            {tab.icon &&
+              React.createElement(tab.icon, { className: "ml-2 h-5 w-5" })}
             {tab.label}
           </TabsTrigger>
         ))}
--- Client/migdalor-admin-panel/src/features/reports/Reports.jsx
+++ Client/migdalor-admin-panel/src/features/reports/Reports.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Sunrise, CalendarClock } from "lucide-react";
 import TabsGroup from "../../components/common/TabsGroup";
 import BokerTovReport from "./BokerTovReport";
 import ActivitiesAndClassesReport from "./ActivitiesAndClassesReport";
@@ -9,11 +10,13 @@ const Reports = () => {
       value: "boker_tov",
       label: "דוח בוקר טוב",
       content: <BokerTovReport />,
+      icon: Sunrise,
     },
     {
       value: "activities_classes",
       label: "דוחות פעילויות וחוגים",
       content: <ActivitiesAndClassesReport />,
+      icon: CalendarClock,
     },
   ];
 
