36fb326155e37a74d3566c70c6c72cdc53b18fe0
RoiCoDA <roishm83@gmail.com>
2025-07-21 00:13:06 +0300

    events layout merge junk

--- Client/MigdalorClient/app/(events)/_layout.jsx
+++ Client/MigdalorClient/app/(events)/_layout.jsx
@@ -2,18 +2,9 @@ import React from "react";
 import { Tabs } from "expo-router";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useTranslation } from "react-i18next";
-<<<<<<< HEAD
-// If you're using translations, you can import it here
-// import { useTranslation } from "react-i18next";
 
 export default function EventsTabLayout() {
   const { t } = useTranslation();
-
-=======
-
-export default function EventsTabLayout() {
-  const { t } = useTranslation();
->>>>>>> RoisMainBranch
 
   return (
     <Tabs
@@ -29,12 +20,7 @@ export default function EventsTabLayout() {
       <Tabs.Screen
         name="Classes"
         options={{
-<<<<<<< HEAD
-          // title: t("Events_ClassesTab"), // Example with translation
-          title: t("Classes"),
-=======
           title: t("Events_ClassesTab"), // Added translation with a fallback
->>>>>>> RoisMainBranch
           tabBarIcon: ({ color }) => (
             <FontAwesome size={30} name="calendar" color={color} />
           ),
@@ -43,12 +29,7 @@ export default function EventsTabLayout() {
       <Tabs.Screen
         name="Activities"
         options={{
-<<<<<<< HEAD
-          // title: t("Events_ActivitiesTab"), // Example with translation
-          title: t("Activities"),
-=======
           title: t("Events_ActivitiesTab"), // Added translation with a fallback
->>>>>>> RoisMainBranch
           tabBarIcon: ({ color }) => (
             <FontAwesome size={30} name="star" color={color} />
           ),
