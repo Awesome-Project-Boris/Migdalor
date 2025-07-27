123dffdb56c3973ff8ed000b7fe607a6c4767ccc
LiorMast <liormasturov@gmail.com>
2025-07-27 17:54:08 +0300

    Update API_BASE_URL to use local server and comment out production URL

--- Client/migdalor-admin-panel/src/config.js
+++ Client/migdalor-admin-panel/src/config.js
@@ -4,6 +4,6 @@
  * Configuration for the application.
  * IMPORTANT: Replace with your actual API server URL.
  */
-// export const API_BASE_URL = "http://192.168.0.160:44315/api";
-export const API_BASE_URL = "https://proj.ruppin.ac.il/cgroup87/prod/api" // Ruppin's server
+export const API_BASE_URL = "http://192.168.0.160:44315/api";
+// export const API_BASE_URL = "https://proj.ruppin.ac.il/cgroup87/prod/api" // Ruppin's server
 // export const API_BASE_URL = "http://192.168.1.234:44315/api";
